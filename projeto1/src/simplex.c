8fc5532f833385ca334df03ff4d73a9a98572f60
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2017-10-17 23:21:12 -0200

    Criando as matrizes basica e nao-basica (terminar)

--- projeto1/src/simplex.c
+++ projeto1/src/simplex.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Definicao das matrizes Basica
+int *defineB(int m, int n, int **A){
+	int **basica;
+	int i;
+	int j;
+
+	//Alocando a matriz B
+	basica = (int*)malloc(m *sizeof (int));
+	for (i=0; i<m; i++)
+		basica[i] = (int) calloc(0, m*sizeof(int));
+
+	for (i=n-1; i>=0; i--)
+		for (j=0; j<m; j++)
+			basica[i][j] = A[i][j];
+
+	return basica;
+}
+
+//Definicao das matrizes Nao-Basica
+int *defineNaoB(int m, int n, int **A){
+	int **Naobasica;
+	int i;
+	int j;
+
+	//Alocando a matriz B
+	Naobasica = (int*)malloc(n *sizeof (int));
+	for (i=0; i<m; i++)
+		Naobasica[i] = (int) calloc(0, (n-m)*sizeof(int));
+
+	for (i=0; i>m; i--)
+		for (j=0; j<n-m; j++)
+			Naobasica[i][j] = A[i][j];
+
+	return Naobasica;
+}
+
 // Essa funcao faz o preenchimento de vetores de forma dinamica
 int *preencheVetor(int tam){
 	int i;
@@ -21,6 +57,8 @@ int main(){
 	int *c; 		// vetor de custos
 	int *b; 		// vetor de recursos
 	int **A;		// matriz de coeficientes
+	int **B;		// matriz basica 
+	int **N;		// matriz nao basica 
 	int i;			//variavel temporaria para criacao dos vetores
 	int j;			//variavel temporaria para criacao dos vetores
 	
@@ -39,9 +77,24 @@ int main(){
 	//matriz de coeficientes
 	A = (int**)malloc(n * sizeof(int));
 	printf("Digite a matriz de coeficientes de restricao.\n");
-	for (i=0; i<n; i++){
+	for (i=0; i<m; i++){
 		printf("Linha %d", i+1);
 		A[i] = preencheVetor(n);
+	}
+	//preenchendo a matriz basica
+	B = defineB(n,m,A);
+	//preenchendo a matriz nao basica
+	N = defineNaoB(n,m,A);
+
+	for (i=0; i<m; i++){
+		for (j=0; j<n; j++)
+			printf("%d ", B[i][j]);
+		printf("\n");
+	}
+	for (i=0; i<m; i++){
+		for (j=0; j<n; j++)
+			printf("%d ", N[i][j]);
+		printf("\n");
 	}
 
 	return 0;	
