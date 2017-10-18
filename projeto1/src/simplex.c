d59c845ffc635f405894f2cc0b0724224085cebf
goldoni <agoldonijr@gmail.com>
2017-10-18 16:07:01 -0200

    foi criado o escopo da maioria das funcoes. Definicao de B e N corrigidas

--- projeto1/src/simplex.c
+++ projeto1/src/simplex.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//multiplicacao de matriz por vetor
+int **multiplicaMatrix(int **M, int **N, int){
+	return mult;
+}
+
+//resolver sistema Xb = (B^-1) b
+int *resolveSistema(int tam, int **matriz, int *vetor){
+	int *xb;
+	return xb;
+}
+
+//resolve lambda
+int *resolveLambda(int *custo, int **B){
+
+	return lambda;
+}
+
+//calcula custo relativo
+int *custosRelativo(int *custo, int *lambda, int **N){
+
+	return custos;
+}
+
+//teste de otimalidade
+int testeOtimalidade(int *custos, int tam){
+	int i;
+
+	for(i=0; i< tam; i++)
+		if(custos[i] < 0)
+			return 0;
+}
 //Definicao das matrizes Basica
 int **defineB(int m, int n, int **A){
 	int **basica;
@@ -11,14 +42,21 @@ int **defineB(int m, int n, int **A){
 	basica = (int*)malloc(m *sizeof (int));
 	for (i=0; i<m; i++)
 		basica[i] = (int) calloc(0, m*sizeof(int));
-
-	for (i=n-1; i>=0; i--)
-		for (j=0; j<m; j++)
-			basica[i][j] = A[i][j];
+	for (i=0; i<m; i++)
+		for (j=0; j<m; j++) {
+			basica[i][j] = A[i][n-m+j];
+		}
 
 	return basica;
 }
+//Define Cb
+int *custoBasico(int *custos){
 
+}
+//Define Cn
+int *custoNaoBasico(int *custos){
+	
+}
 //Definicao das matrizes Nao-Basica
 int **defineNaoB(int m, int n, int **A){
 	int **Naobasica;
@@ -30,7 +68,7 @@ int **defineNaoB(int m, int n, int **A){
 	for (i=0; i<m; i++)
 		Naobasica[i] = (int) calloc(0, (n-m)*sizeof(int));
 
-	for (i=0; i>m; i--)
+	for (i=0; i<m; i++)
 		for (j=0; j<n-m; j++)
 			Naobasica[i][j] = A[i][j];
 
@@ -59,8 +97,13 @@ int main(){
 	int **A;		// matriz de coeficientes
 	int **B;		// matriz basica 
 	int **N;		// matriz nao basica 
-	int i;			//variavel temporaria para criacao dos vetores
-	int j;			//variavel temporaria para criacao dos vetores
+	int *cb;		// custo basico
+	int *cn;		// custo nao basico
+	int *xb;		// vetor de solucao basica
+	int *lambda;	// vetor multiplicador simplex
+	int *cRelativo;	// custos relativos
+	int i;			// variavel temporaria para criacao dos vetores
+	int j;			// variavel temporaria para criacao dos vetores
 	
 	//Numero de linhas e colunas 
 	printf("Digite o numero de linhas e colunas.\n");
@@ -81,20 +124,20 @@ int main(){
 		printf("Linha %d", i+1);
 		A[i] = preencheVetor(n);
 	}
-	//preenchendo a matriz basica
+	//preenchendo a matriz basica e nao basica
 	B = defineB(m,n,A);
-	//preenchendo a matriz nao basica
 	N = defineNaoB(m,n,A);
+	
+	//preenche o vetor custoBasico e custoNaoBasico
+	cn = custoNaoBasico();
+	cb = custoBasico();
 
-	for (i=0; i<m; i++){
-		for (j=0; j<m; j++)
-			printf("%d ", B[i][j]);
-		printf("\n");
-	}
-	for (i=0; i<m; i++){
-		for (j=0; j<n-m; j++)
-			printf("%d ", N[i][j]);
-		printf("\n");
+	while(true){
+		//calcula Xb
+		xb = resolveSistema(m,B,b);
+		lambda = resolveSistema(m,B,c);
+		cRelativo = custosRelativo()
+		break;
 	}
 
 	return 0;	
